cb3b0aaab19e93bd97a9e6850fecf42a66978ac3
Yuta Fujiyama <yuta.technology@gmail.com>
2024-07-04 13:43:11 +0900

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+// src/App.js
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/hello')
+      .then(response => {
+        // console.log('Response:', response.text());
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Response data:', data);
+        setMessage(data.message);
+      })
+      .catch(error => {
+        console.error('Error fetching message:', error.message);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
